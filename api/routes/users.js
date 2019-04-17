e1478c2cf87348afd46ac1018cc4ec9656d786fa
Gustavo Maronato <gustavomaronato@gmail.com>
2019-04-17 02:42:00 -0300

    added auth retrieval api endpoint

--- api/routes/users.js
+++ api/routes/users.js
@@ -16,4 +16,25 @@ router.post('/users/update/', (req, res) => {
     res.send()
 })
 
+router.get('/users/get/auth/', (req, res) => {
+    try {
+        let data = User.findOne().byUserId(req.query.user_id).then(user => {
+            res.json({
+                access_token: user.access_token,
+                refresh_token: user.refresh_token
+            })
+            res.status(200)
+            res.send()
+        }).catch(err => {
+            res.status(404)
+            res.send()
+        })
+    } catch (err) {
+        res.status(400)
+        Sentry.captureException(err)
+        console.log(err)
+        res.send()
+    }
+})
+
 export default router
