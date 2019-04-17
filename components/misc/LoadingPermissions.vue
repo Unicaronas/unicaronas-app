77d4bf62e0d64646637928bc1273df6dd6cf3363
Gustavo Maronato <gustavomaronato@gmail.com>
2019-04-17 14:40:17 -0300

    fix mount loading

--- components/misc/LoadingPermissions.vue
+++ components/misc/LoadingPermissions.vue
@@ -1,21 +1,25 @@
 <template>
-    <v-layout align-center justify-center>
-        <v-layout row wrap>
-            <v-flex d-flex xs12>
-                <h1 class="display-1 font-weight-thin mb-3 text-xs-center">
-                    Carregando permissões...
-                </h1>
-            </v-flex>
-            <v-flex d-flex xs12>
-                <v-progress-circular
-                :size="70"
-                :width="7"
-                color="blue"
-                indeterminate
-                />
-            </v-flex>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-layout row wrap>
+                <v-flex d-flex xs12>
+                    <h1
+                    class="display-1 font-weight-thin mb-3 text-xs-center"
+                    >
+                        Carregando permissões...
+                    </h1>
+                </v-flex>
+                <v-flex d-flex xs12>
+                    <v-progress-circular
+                    :size="70"
+                    :width="7"
+                    color="blue"
+                    indeterminate
+                    />
+                </v-flex>
+            </v-layout>
         </v-layout>
-    </v-layout>
+    </v-container>
 </template>
 <script>
     export default {
